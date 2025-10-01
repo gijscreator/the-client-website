dd32c159118ebe2320266074c25921d81e51153a
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-10-01 22:34:59 +0200

    100vw header

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 const button = document.querySelector(".gridselector");
-
 const grid = document.querySelector(".images-grid");
 const list = document.querySelector(".images-list");
 
-button.addEventListener("click", function () {
+function toggleView() {
   const listIsActive = list.classList.contains("active");
 
   if (listIsActive) {
+
     list.classList.remove("active");
     grid.classList.add("active");
-
     button.classList.remove("list-active");
-
-    console.log("Now showing: GRID");
   } else {
+
     grid.classList.remove("active");
     list.classList.add("active");
-
     button.classList.add("list-active");
-
-    console.log("Now showing: LIST");
   }
-});
+}
+
+
+button.addEventListener("click", toggleView);
+
+button.addEventListener("pointerup", toggleView);
