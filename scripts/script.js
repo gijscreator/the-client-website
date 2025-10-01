5e1b13a54f86d609b2b6815c6aabd12ed0357efc
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-10-01 22:37:33 +0200

    removed pointerup

--- scripts/script.js
+++ scripts/script.js
@@ -6,19 +6,16 @@ function toggleView() {
   const listIsActive = list.classList.contains("active");
 
   if (listIsActive) {
-
     list.classList.remove("active");
     grid.classList.add("active");
     button.classList.remove("list-active");
+    console.log("Now showing GRID");
   } else {
-
     grid.classList.remove("active");
     list.classList.add("active");
     button.classList.add("list-active");
+    console.log("Now showing LIST");
   }
 }
 
-
 button.addEventListener("click", toggleView);
-
-button.addEventListener("pointerup", toggleView);
