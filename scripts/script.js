3fc97a4f55ca633ce57cc91110f811d5e0512860
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-10-01 20:44:12 +0200

    dropdown menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-// JS
+document.addEventListener("click", function(event) {
+
+  let clickedElement = event.target.closest(".gridselector");
+  let element = document.querySelector(".images-list");
+
+  if (!clickedElement) {
+    return;
+  }
+
+  element.style.display = "none";
+});
