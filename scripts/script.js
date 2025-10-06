f0734eeb466b9780667f6a5aaf5152897567a3e3
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-10-06 11:14:55 +0200

    r

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,15 @@ const list = document.querySelector(".images-list");
 function toggleView() {
   const listIsActive = list.classList.contains("active");
 
+  // Grid view active
   if (listIsActive) {
     list.classList.remove("active");
     grid.classList.add("active");
     button.classList.remove("list-active");
     console.log("Now showing GRID");
+
+    // List view active
+
   } else {
     grid.classList.remove("active");
     list.classList.add("active");
